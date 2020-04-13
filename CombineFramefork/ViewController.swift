0bf31e0c7d805e3c1d87ad46a9b20cfb752b695d
Ruslan <ruslanfilistovich@gmail.com>
2020-04-13 17:12:51 +0300

    fr combine

--- CombineFramefork/ViewController.swift
+++ CombineFramefork/ViewController.swift
@@ -7,14 +7,49 @@
 //
 
 import UIKit
+import Combine
+
+struct BlogPost {
+    let title: String
+    let url: URL
+}
+
+extension Notification.Name {
+    static let newBlogPost = Notification.Name("newBlogPost")
+}
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var acceptSwitch: UISwitch!
+    @IBOutlet var makePostButton: UIButton!
+    @IBOutlet weak var postLabel: UILabel!
+    
+    @Published var canMakePost: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        $canMakePost.receive(on: DispatchQueue.main).assign(to: \.isEnabled, on: makePostButton)
+        
+        setupCombine()
+    }
+    
+    private func setupCombine() {
+        let blogPostPublisher = NotificationCenter.Publisher(center: .default, name: .newBlogPost, object: nil).map({ (notification) -> String? in
+            return (notification.object as? BlogPost)?.title ?? ""
+        })
+        let postLabelSubscriber = Subscribers.Assign(object: postLabel, keyPath: \.text)
+        
+        blogPostPublisher.subscribe(postLabelSubscriber)
     }
 
 
+    @IBAction func toggleSwitch(_ sender: UISwitch) {
+        canMakePost = sender.isOn
+    }
+    
+    @IBAction func makePostButtonTap(_ sender: UIButton) {
+        let blogPost = BlogPost(title: "The blog post\nNow \(Date())", url: URL(string: "Swift.org")!)
+        NotificationCenter.default.post(name: .newBlogPost, object: blogPost)
+    }
 }
 
